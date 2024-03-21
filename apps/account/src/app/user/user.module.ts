96922f8490f7e0499248f5ade569eecdd070c6aa
LightninG8 <tov.kessler@yanedx.ru>
2024-03-21 23:07:10 +0300

    feat: Добавлена простая авторизация

--- apps/account/src/app/user/user.module.ts
+++ apps/account/src/app/user/user.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
-import { UserController } from './user.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './models/user.model';
+import { UserRepository } from './repositories/user.repository';
 
 @Module({
-  providers: [UserService],
-  controllers: [UserController],
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  providers: [UserRepository],
+  exports: [UserRepository],
 })
 export class UserModule {}
